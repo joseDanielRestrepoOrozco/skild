0a56fa7905487ee89f282800cadc6ac1ab2c9482
joseDanielRestrepoOrozco <jose.resoro4789@gmail.com>
2026-05-20 09:32:52 -0500

    fix:reder 'Unknow data' when createdAt in SkillCard when is null, fix:bug when handleCopy raise error, add: theme init script to root

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -21,9 +21,13 @@ const SkillCard = ({
 	const [copied, setCopied] = useState(false);
 
 	const handleCopy = () => {
-		navigator.clipboard.writeText(installCommand);
-		setCopied(true);
-		setTimeout(() => setCopied(false), 2000);
+		try {
+			navigator.clipboard.writeText(installCommand);
+			setCopied(true);
+			setTimeout(() => setCopied(false), 2000);
+		} catch {
+			setCopied(false);
+		}
 	};
 
 	return (
@@ -50,7 +54,11 @@ const SkillCard = ({
 						<img src="/logo512.png" alt="author avatar" className="avatar" />
 						<div className="author-copy">
 							<p>Adrian</p>
-							<p>{new Date(createdAt as string).toLocaleDateString()}</p>
+							<p>
+								{createdAt
+									? new Date(createdAt as string).toLocaleDateString()
+									: 'Unknown date'}
+							</p>
 						</div>
 					</div>
 					<p className="category">{category}</p>
